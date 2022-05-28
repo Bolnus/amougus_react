d0c0693a462063f35bf596d797f96766697f5225
Bolnus <you@example.com>
2022-05-28 14:15:20 +0300

    изменено:
    - фикс вызова getplace() в store
    
    - удалены лишние старые функции в state.jsx

--- src/redux/state.jsx
+++ src/redux/state.jsx
@@ -45,6 +45,10 @@ let store = {
     {
         return this._state;
     },
+    subscribe(observer)
+    {
+        rerenderEntireTree=observer;
+    },
     getPlace(resultValue)
     {
         //let inserted=0;
@@ -80,7 +84,7 @@ let store = {
             "time": currentTime,
             "result": result
         }
-        let placeNumber = getPlace(result);
+        let placeNumber = this.getPlace(result);
         if(placeNumber<maxRecords)
         {
             newNewRecord.place=placeNumber;
@@ -94,7 +98,7 @@ let store = {
     {
         if(this._state.newRecord.hasOwnProperty("result"))
         {
-            this._state.newRecord.place = getPlace(this._state.newRecord.result);
+            this._state.newRecord.place = this.getPlace(this._state.newRecord.result);
             if (this._state.newRecord.place < maxRecords)
             {
                 let newNewRecord = {
@@ -118,142 +122,10 @@ let store = {
     {
         this._state.newRecord.name=newName;
         rerenderEntireTree(this);
-    },
-    subscribe(observer)
-    {
-        rerenderEntireTree=observer;
     }
+
 }
 
-
-
-let state = {
-    recordsData: [
-        {
-            "name": "AAA",
-            "date": "5.1.2022",
-            "time": "17:17:58",
-            "result": "00-06-120"
-        },
-        {
-            "name": "AAA",
-            "date": "5.1.2022",
-            "time": "17:17:58",
-            "result": "00-06-144"
-        },
-        {
-            "name": "AAA",
-            "date": "6.1.2022",
-            "time": "18:44:39",
-            "result": "00-06-310"
-        },
-        {
-            "name": "AAA",
-            "date": "6.1.2022",
-            "time": "18:47:29",
-            "result": "00-06-410"
-        },
-        {
-            "name": "AAA",
-            "date": "6.1.2022",
-            "time": "18:58:1",
-            "result": "00-08-805"
-        }],
-    newRecord: {
-
-    }
-    //subPage: "start"
-}
-
-function getPlace(resultValue)
-{
-    //let inserted=0;
-    let intResult = parseInt(resultValue.replace(/-/g,''),10);
-    if(intResult<parseInt(state.recordsData[0].result.replace(/-/g,''),10))
-    {
-        return 0;
-    }
-    for(let i=0;i<state.recordsData.length-1;i++)
-    {
-        let result1 = parseInt(state.recordsData[i].result.replace(/-/g,''),10);
-        let result2 = parseInt(state.recordsData[i+1].result.replace(/-/g,''),10);
-
-        if(intResult>result1&&intResult<result2)
-        {
-            //state.recordsData.splice(i+1, 0, newRecord);
-            //inserted=1;
-            return i+1;
-            //newNewRecord.name = "AAA";
-            //state.newRecord = newNewRecord;
-        }
-    }
-    return state.recordsData.length;
-}
-
-export function prepareRecord(result)
-{
-    let date = new Date();
-    let currentTime = date.getHours()+':'+date.getMinutes()+':'+date.getSeconds();
-    let month = date.getMonth()+1;
-    let currentDate = date.getDate()+'.'+month+'.'+date.getFullYear();
-    let newNewRecord = {
-        "date": currentDate,
-        "time": currentTime,
-        "result": result
-    }
-    let placeNumber = getPlace(result);
-    if(placeNumber<maxRecords)
-    {
-        newNewRecord.place=placeNumber;
-        newNewRecord.name = "AAA";
-    }
-    state.newRecord = newNewRecord;
-    rerenderEntireTree(state);
-    //return 1;
-}
-
-export function writeRecord()
-{
-    if(state.newRecord.hasOwnProperty("result"))
-    {
-        state.newRecord.place = getPlace(state.newRecord.result);
-        if(state.newRecord.place<maxRecords)
-        {
-            let newNewRecord = {
-                "name": state.newRecord.name,
-                "date": state.newRecord.date,
-                "time": state.newRecord.time,
-                "result": state.newRecord.result
-            }
-            state.recordsData.splice(state.newRecord.place, 0, newNewRecord);
-            if(state.recordsData.length>maxRecords)
-                state.recordsData.pop();
-        }
-    }
-    //rerenderEntireTree(state);
-}
-
-export function clearRecord()
-{
-    state.newRecord = {};
-    rerenderEntireTree(state);
-}
-
-export function updateNewRecordName(newName)
-{
-    state.newRecord.name=newName;
-    rerenderEntireTree(state);
-}
-
-export function updateRecords()
-{
-    rerenderEntireTree(state);
-}
-
-export function subscribe(observer)
-{
-    rerenderEntireTree=observer;
-}
 
 window.store=store;
 
